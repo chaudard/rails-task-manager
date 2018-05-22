3aac34d9c248c132675359c3baadc3b75990577c
chaudard <donnetonfalzard+github@gmail.com>
2018-05-22 16:46:52 +0200

    using resources :tasks in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'tasks', to: "tasks#index"
-  get "tasks/new", to: "tasks#new", as: :new
-  get "tasks/:id", to: "tasks#show"
-  post "tasks", to: "tasks#create"
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
-  patch "tasks/:id", to: "tasks#update", as: :task
-  delete "tasks/:id", to: "tasks#destroy", as: :delete
+  # get 'tasks', to: "tasks#index"
+  # get "tasks/new", to: "tasks#new", as: :new
+  # get "tasks/:id", to: "tasks#show"
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  # patch "tasks/:id", to: "tasks#update", as: :task
+  # delete "tasks/:id", to: "tasks#destroy", as: :delete
+    resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
